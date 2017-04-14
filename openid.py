d4d097308f4525801d467be3e6991e2d07ad01bb
Woon Kiat Wong <wowong@microsoft.com>
2017-04-13 17:20:57 -0700

    Update the code sample with comments from the code review.

--- openid.py
+++ openid.py
@@ -1,53 +1,102 @@
 # 
-# Copyright (c) Microsoft. All rights reserved.
-# Licensed under the MIT license.
-# 
-# Copyright (c) Microsoft Corporation
+# Copyright (c) Microsoft Corporation.
 # All rights reserved.
 # 
-# MIT License:
-# Permission is hereby granted, free of charge, to any person obtaining
-# a copy of this software and associated documentation files (the
-# "Software"), to deal in the Software without restriction, including
-# without limitation the rights to use, copy, modify, merge, publish,
-# distribute, sublicense, and/or sell copies of the Software, and to
-# permit persons to whom the Software is furnished to do so, subject to
-# the following conditions:
+# This code is licensed under the MIT License.
 # 
-# The above copyright notice and this permission notice shall be
-# included in all copies or substantial portions of the Software.
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files(the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions :
 # 
-# THE SOFTWARE IS PROVIDED ""AS IS"", WITHOUT WARRANTY OF ANY KIND,
-# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+# 
 
 import base64
 import binascii
+import datetime
 import json
 import urllib.request
 
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import rsa
+from datetime import (datetime, timedelta)
 
 class OpenIDConnectConfiguration(object):
-    def __init__(self, url):
-        self.load_config(url)
+    """
+    Represents an OpenID configuration.
+    """
     
-    def load_config(self, url):
+    lastUpdated = datetime.now()
+    signing_keys = {}
+
+    def __init__(self, url):
+        """
+        Construtor.
+        """
+        self._url = url
+        keys = OpenIDConnectConfiguration.signing_keys.get(url)
+        if keys is None:
+            OpenIDConnectConfiguration.refresh_cache(url)
+
+    def get_key(self, keyId):
+        """
+        Gets the public key from the cache given the key ID.
+        """
+        diff = datetime.now() - OpenIDConnectConfiguration.lastUpdated
+
+        # Refresh the cache if it's more than 5 days old.
+        if diff.total_seconds() > 5 * 24 * 60 * 60:
+            refresh_cache()
+
+        keys  = OpenIDConnectConfiguration.signing_keys.get(self._url)
+        if keys is not None:
+            return keys.get(keyId)
+            
+        return None
+
+    @staticmethod
+    def refresh_cache(url):
+        """
+        Refresh the keys cache.
+        """
+        openid_config = OpenIDConnectConfiguration.load_config(url)
+        jwks_uri = openid_config['jwks_uri']
+        keys = OpenIDConnectConfiguration.load_jwks(jwks_uri)
+        
+        OpenIDConnectConfiguration.signing_keys[url] = keys
+
+    @staticmethod
+    def load_config(url):
+        """
+        Load the Open ID configuration from the given URL.
+        """
         req = urllib.request.urlopen(url)
         openid_config = json.loads(req.read().decode('utf-8'))
-        jwks_uri = openid_config['jwks_uri']
-        self.load_jwks(jwks_uri)
+        
+        return openid_config
+        # jwks_uri = openid_config['jwks_uri']
+        # self.load_jwks(jwks_uri)
     
-    def load_jwks(self, url):
+    @staticmethod
+    def load_jwks(url):
+        """
+        Loads the JSON web key set from the given URL.
+        """
         req = urllib.request.urlopen(url)
         jwks = json.loads(req.read().decode('utf-8'))
-        self._signing_keys = {}
+        keys = {}
         
         for key in jwks['keys']:
             exp_b64 = key['e']
@@ -56,12 +105,14 @@ class OpenIDConnectConfiguration(object):
             mod = int(binascii.hexlify(base64.urlsafe_b64decode(pad_base64_str(modulus_b64))), 16)
             pub_num = rsa.RSAPublicNumbers(exp, mod)
             public_key = pub_num.public_key(default_backend())
-            self._signing_keys[key['kid']] = public_key
+            keys[key['kid']] = public_key
         
-    def signing_keys(self):
-        return self._signing_keys
-
+        return keys
+        
 def pad_base64_str(str):
+    """
+    Pads the base64 string.
+    """
     missing_padding = len(str) % 4
     if missing_padding != 0:
         str += '=' * (4 - missing_padding)
--- validation.py
+++ validation.py
@@ -1,30 +1,27 @@
 # 
-# Copyright (c) Microsoft. All rights reserved.
-# Licensed under the MIT license.
-# 
-# Copyright (c) Microsoft Corporation
+# Copyright (c) Microsoft Corporation.
 # All rights reserved.
 # 
-# MIT License:
-# Permission is hereby granted, free of charge, to any person obtaining
-# a copy of this software and associated documentation files (the
-# "Software"), to deal in the Software without restriction, including
-# without limitation the rights to use, copy, modify, merge, publish,
-# distribute, sublicense, and/or sell copies of the Software, and to
-# permit persons to whom the Software is furnished to do so, subject to
-# the following conditions:
+# This code is licensed under the MIT License.
 # 
-# The above copyright notice and this permission notice shall be
-# included in all copies or substantial portions of the Software.
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files(the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions :
 # 
-# THE SOFTWARE IS PROVIDED ""AS IS"", WITHOUT WARRANTY OF ANY KIND,
-# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+# 
 
 import base64
 import binascii
@@ -37,60 +34,105 @@ from cryptography.hazmat.primitives.asymmetric import rsa
 from openid import OpenIDConnectConfiguration
 
 class InvalidActionableMessageTokenError(Exception) : 
+    """
+    Represents the error where the actionable message token is invalid.
+    """
     pass
     
 class ActionableMessageTokenValidationResult(object):
+    """
+    Result from a token validation.
+    """
+    
     def __init__(self):
+        """
+        Constructor.
+        """
         self.__sender = ''
         self.__action_performer = ''
-        
+
     @property
     def sender(self):
+        """
+        Gets the sender.
+        """
         return self.__sender
         
     @sender.setter
     def sender(self, value):
+        """
+        Sets the sender.
+        """
         self.__sender = value
     
     @sender.deleter
     def sender(self):
+        """
+        Deletes the sender property.
+        """
         del self.__sender
 
     @property
     def action_performer(self):
+        """
+        Gets the action performer.
+        """
         return self.__action_performer
         
     @action_performer.setter
     def action_performer(self, value):
+        """
+        Sets the action performer.
+        """
         self.__action_performer = value
     
     @action_performer.deleter
     def action_performer(self):
+        """
+        Deletes the action performer property.
+        """
         del self.__action_performer
 
+class O365OpenIdConfiguration(object):
+    """
+    Constants for O365 Open ID configuration.
+    """
+    APP_ID = "48af08dc-f6d2-435f-b2a7-069abd99c086";
+    METADATA_URL = "https://substrate.office.com/sts/common/.well-known/openid-configuration";
+    TOKEN_ISSUER = "https://substrate.office.com/sts/";
+        
 class ActionableMessageTokenValidator(object):
-    def __init__(self): pass
+    """
+    The validator for actionable message token.
+    """
+    
+    def __init__(self): 
+        """
+        Constructor.
+        """
+        pass
     
     def validation_token(self, token, targetUrl):
+        """
+        Validate the JWT token issued by Microsoft.
+        """
         try:
             result = ActionableMessageTokenValidationResult()
-            openid_config = OpenIDConnectConfiguration('https://substrate.office.com/sts/common/.well-known/openid-configuration')
-            signing_keys = openid_config.signing_keys()
+            openid_config = OpenIDConnectConfiguration(O365OpenIdConfiguration.METADATA_URL)
             header = self._get_jwt_header(token)
+            public_key = openid_config.get_key(header['kid'])
             
-            if header['kid'] not in signing_keys.keys():
+            if public_key is None:
                 raise InvalidActionableMessageTokenError('KeyID {:s} does not exist'.format(header['kid']))
-            
-            public_key = signing_keys[header['kid']]
             
             claims = jwt.decode(
                 token, 
                 public_key, 
                 algorithms=['RS256'], 
-                issuer='https://substrate.office.com/sts/',
+                issuer=O365OpenIdConfiguration.TOKEN_ISSUER,
                 audience=targetUrl)
             
-            if claims['appid'].lower() != '48af08dc-f6d2-435f-b2a7-069abd99c086':
+            if claims['appid'].lower() != O365OpenIdConfiguration.APP_ID:
                 raise InvalidActionableMessageTokenError('Invalid appid in the token')
 
             result.sender = claims['sender']
@@ -101,6 +143,9 @@ class ActionableMessageTokenValidator(object):
         return result
         
     def _get_jwt_header(self, jwt):
+        """
+        Gets the header of the JWT token.
+        """
         jwt = jwt.encode('utf-8')
         
         signing_input, crypto_segment = jwt.rsplit(b'.', 1)
